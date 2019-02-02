5deb019e48ffd17e655e6885528f30dd22218afb
Artem Yurev <artemon-yrev@yandex.ru>
2019-02-02 17:22:06 +0300

    level1.step6

---
+++ level1/step6/index.php
@@ -0,0 +1,16 @@
+<?php
+$nameOne = 'Jeff';
+$nameTwo = 'John';
+
+$names  = [
+    'Jeff',
+    'John',
+    'Mary'
+];
+
+foreach ($names as $name)
+{
+    echo $name;
+}
+
+require 'index.view.php';
