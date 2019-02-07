6f87efe5602e91e63535ee513e7e561cd3be7642
Artem <artemon-yrev@yandex.ru>
2019-02-07 13:23:27 +0300

    level1/step9/example1

---
+++ level1/step9/index.php
@@ -0,0 +1,11 @@
+<?php
+$task = [
+    'title' => 'Finish homework',
+    'due' => 'today',
+    'assigned_to' => 'Jeffrey',
+    'completed' => false
+];
+
+print_r($task);
+
+require 'index.view.php'; 
---
+++ level1/step9/index.view.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <li>
+        <strong>title: </strong>
+        <span><?= $task['title']; ?></span>
+    </li>
+    <li>
+        <strong>due date: </strong>
+        <span><?= $task['due']; ?></span>
+    </li>    
+    <li>
+        <strong>personal responsibility: </strong>
+        <span><?= $task['assigned_to']; ?></span>
+    </li>    
+    <li>
+        <strong>status: </strong>
+        <?php if($task['completed']):?>
+        <span><?= 'completed' ?></span>
+        <?php else: ?>
+        <span><?= 'in process' ?></span>
+        <?php endif ?>
+    </li>    
+</body>
+</html>
