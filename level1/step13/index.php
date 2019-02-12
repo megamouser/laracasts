af23830bef28d3230aaef5943bb1501122b57ffc
Artem <artemon-yrev@yandex.ru>
2019-02-12 17:43:47 +0300

    level1/step13/example5

--- level1/step13/index.php
+++ level1/step13/index.php
@@ -1,18 +1,9 @@
 <?php
+require 'functions.php';
 require 'Task.php';
 
-try {
-    $pdo = new PDO('mysql:host=127.0.0.1;dbname=mytodo', 'admin', 'd365ef90c1211fdd6fbfb6eb79eb37b0');
-} catch (PDOException $e) {
-    die($e->getMessage());
-}
-
-$statement = $pdo->prepare('select * from todos');
-$statement->execute();  
-
-$tasks = ($statement->fetchAll(PDO::FETCH_CLASS, 'Task'));
-var_dump($tasks[0]->foobar());
-
+$pdo = connectToDb();
+$tasks = 
 require 'index.view.php';
 
 
