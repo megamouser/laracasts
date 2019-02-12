a0acac375b9427a3048559ab2603ca5f8d488b04
Artem <artemon-yrev@yandex.ru>
2019-02-12 16:59:24 +0300

    level1/step13/example3

--- level1/step13/index.php
+++ level1/step13/index.php
@@ -2,14 +2,15 @@
 try {
     $pdo = new PDO('mysql:host=127.0.0.1;dbname=mytodo', 'admin', 'd365ef90c1211fdd6fbfb6eb79eb37b0');
 } catch (PDOException $e) {
-    die('Could not connect');
+    die($e->getMessage());
 }
 
 $statement = $pdo->prepare('select * from todos');
 $statement->execute();  
 
-$results = ($statement->fetchAll(PDO::FETCH_OBJ));
-var_dump($results[0]->description);
+$tasks = ($statement->fetchAll(PDO::FETCH_OBJ));
+
+require 'index.view.php';
 
 
 
