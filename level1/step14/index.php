8670f76d6b48b3d21f70db410b9304db0a9ea5bf
Artem <artemon-yrev@yandex.ru>
2019-02-13 11:58:04 +0300

    level1/step14/example2

--- level1/step14/index.php
+++ level1/step14/index.php
@@ -1,10 +1,11 @@
 <?php
-require 'functions.php';
 require 'database/Connection.php';
+require 'database/QueryBuilder.php';
 require 'Task.php';
 
 $pdo = Connection::make();
-$tasks = fetchAllTasks($pdo);
+$query = new QueryBuilder($pdo);
+$tasks = $query->selectAll('todos');
 
 require 'index.view.php';
 
