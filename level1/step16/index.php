248d4439386bb3341ac642f07c26ab4fc9e27f8f
Artem Yurev <artemon-yrev@yandex.ru>
2019-02-17 18:57:01 +0300

    level1/step16/example3

--- level1/step16/index.php
+++ level1/step16/index.php
@@ -3,5 +3,5 @@ $database = require 'core/bootstrap.php';
 
 $uri = trim($_SERVER['REQUEST_URI'], '/');
 
-$router = Router::load('routes.php');
-require $router->direct($uri);
+require Router::load('routes.php')->direct($uri);
+
