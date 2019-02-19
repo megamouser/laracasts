c60555e78a4ff9246577376c79ff07ce0a18d415
Artem <artemon-yrev@yandex.ru>
2019-02-19 18:09:21 +0300

    level1/step18/example4

--- level1/step18/index.php
+++ level1/step18/index.php
@@ -2,20 +2,22 @@
 class Post 
 {
     public $title;
+    public $author;
     public $published;
 
-    public function __construct($title, $published)
+    public function __construct($title, $author, $published)
     {
         $this->title = $title;
+        $this->author = $author;
         $this->published = $published;
     }
 }
 
 $posts = [
-    new Post('My First Post', true),
-    new Post('My Second Post', true),
-    new Post('My Third Post', true),
-    new Post('My Fourth Post', false)
+    new Post('My First Post', 'JW', true),
+    new Post('My Second Post', 'JW', true),
+    new Post('My Third Post', 'AW', true),
+    new Post('My Fourth Post', 'TR', false)
 ];
 
 foreach ($posts as $post) 
@@ -64,8 +66,13 @@ $titles = array_map(function($post) {
 }, $posts);
 */
 
+/** 
 $posts = array_map(function($post) {
     return (array) $post;
 }, $posts);
+*/
 
-var_dump($posts);
+//$titles = array_column($posts, 'title');
+
+$authors = array_column($posts, 'author', 'title');
+var_dump($authors);
