e57d4b3d3fcc73d06df7e0799a95c4bf1492ad9c
Artem <artemon-yrev@yandex.ru>
2019-02-20 12:07:24 +0300

    level1/step19/example

---
+++ level1/step19/config.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'database' => [
+        'name' => 'mytodo',
+        'username' => 'admin',
+        'password' => 'd365ef90c1211fdd6fbfb6eb79eb37b0',
+        'connection' => 'mysql:host=127.0.0.1',
+        'options' => [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ]
+    ]
+];
---
+++ level1/step19/views/partials/nav.php
@@ -0,0 +1,9 @@
+<nav>
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/about/culture">About our culture</a></li>
+        <li><a href="/contact">Contacts</a></li>
+    </ul>
+</nav>
+
