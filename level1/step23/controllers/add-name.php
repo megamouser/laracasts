6baa1de31eebf5b0ca9ae0c3d306a4e2d01aba6a
Artem Yurev <artemon-yrev@yandex.ru>
2019-02-26 22:00:50 +0300

    level1/step22/example1

---
+++ level1/step23/controllers/add-name.php
@@ -0,0 +1,13 @@
+<?php
+App::get('database')->insert('users', [
+    'name' => $_POST['name']
+]);
+
+header('Location: /');
+
+
+
+ 
+
+
+
---
+++ level1/step23/routes.php
@@ -0,0 +1,13 @@
+<?php
+
+// $router->define([
+//     '' => 'controllers/index.php',
+//     'about' => 'controllers/about.php',
+//     'about/culture' => 'controllers/about-culture.php',
+//     'contact' => 'controllers/contact.php',
+//     'names' => 'controllers/add-name.php', // only for post types
+// ]);
+
+$router->get('', 'PagesController@home');
+$router->get('about', 'PagesController@about');
+$router->get('contact', 'PagesController@contact');
