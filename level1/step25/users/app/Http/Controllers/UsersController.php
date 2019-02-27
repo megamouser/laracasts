9fb25acd148a99346d1cefe3cc51220e8ee40bc8
Artem Yurev <artemon-yrev@yandex.ru>
2019-02-27 22:19:00 +0300

    level1/step25/example2

--- level1/step25/users/app/Http/Controllers/UsersController.php
+++ level1/step25/users/app/Http/Controllers/UsersController.php
@@ -1,13 +1,26 @@
 <?php
 
 namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
+use App\User;
 
 class UsersController extends Controller
 {
     public function index()
     {
-        return view('users.index');
+        // select * from users
+        
+        $users = User::all();
+        return view('users.index', compact('users'));
+    }
+
+    public function store()
+    {
+        $user = new User;
+        $user->name = request('name');
+        $user->email = request('email');
+        $user->password = bcrypt(request('password'));
+        $user->save();
+        
+        return back();
     }
 }
--- level1/step25/users/resources/views/users/index.blade.php
+++ level1/step25/users/resources/views/users/index.blade.php
@@ -8,5 +8,18 @@
 </head>
 <body>
     <h1>All Users</h1>
+    @foreach ($users as $user)
+        <li>{{ $user->name }}</li>
+    @endforeach
+
+    <h2>Add a User</h2>
+
+    <form method="POST" action="/users">
+        {{ csrf_field() }}
+        <p><input type="text" name="name" placeholder="Name" required></p>
+        <p><input type="email" name="email" placeholder="Email Address" required></p>
+        <p><input type="password" name="password" placeholder="Password" required></p>
+        <p><button type="submit">Add User</button></p>
+    </form>
 </body>
 </html>
