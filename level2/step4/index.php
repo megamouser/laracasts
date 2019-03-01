b7559d3818e1f8f1d20440a5176273c020e23eba
Artem <artemon-yrev@yandex.ru>
2019-03-01 10:26:51 +0300

    level2/step4/example1

---
+++ level2/step4/index.php
@@ -0,0 +1,28 @@
+<?php
+class Shape
+{
+    protected $length = 4;
+
+    public function getArea()
+    {
+        return pow($this->length, 2);
+    }
+}
+
+class Square extends Shape
+{
+}
+
+class Triangle extends Shape
+{
+    protected $base = 4;
+    protected $height = 7;
+
+    public function getArea()
+    {
+        return .5 * $this->base * $this->height;
+    }
+}
+
+echo (new Square)->getArea();
+echo (new Triangle)->getArea();
