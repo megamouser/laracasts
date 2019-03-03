b33fcbe165be2670c2004dedb1caf2cac787ddc3
Artem Yurev <artemon-yrev@yandex.ru>
2019-03-03 06:51:30 +0300

    level2/step6/example2

---
+++ level2/step6/ex.php
@@ -0,0 +1,12 @@
+<?php
+require 'src/Person.php';
+require 'src/Business.php';
+require 'src/Staff.php';
+
+$jeffrey = new Person('Jeffrey Way');
+$staff = new Staff([$jeffrey]);
+$laracasts = new Business($staff);
+$laracasts->hire(new Person('Jane Doe'));
+var_dump($laracasts->getStaffMembers());
+
+
