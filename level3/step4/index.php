12c42f91fd5f1ca2167c9f5a4b782d0993c8d3c4
Artem Yurev <artemon-yrev@yandex.ru>
2019-03-09 19:40:03 +0300

    level3/step4/example1

---
+++ level3/step4/index.php
@@ -0,0 +1,43 @@
+<?php
+interface Logger
+{
+    public function log($data);
+}
+
+class LogToFile implements Logger
+{
+    public function log($data)
+    {
+        var_dump('Log the data to a file');
+    }
+}
+
+class LogToDatabase implements Logger
+{
+    public function log($data)
+    {
+        var_dump('log the data to database');
+    }
+}
+
+class LogToXWebService implements Logger
+{
+    public function log($data)
+    {
+        var_dump('Log the data to a XWebService');
+    }
+}
+
+class App
+{
+    public function log($data, Logger $logger)
+    {
+
+        $logger->log($data);
+    }
+}
+
+$app = new App;
+$app->log('Some information here', new LogToXWebService);
+$app->log('Some information here', new LogToDatabase);
+$app->log('Some information here', new LogToFile);
