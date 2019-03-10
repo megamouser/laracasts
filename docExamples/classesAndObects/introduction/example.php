b2acad19200a29fb8454bd2b79716046dc3baf20
Artem Yurev <artemon-yrev@yandex.ru>
2019-03-11 00:27:49 +0300

    docExamples/example2

--- docExamples/classesAndObects/introduction/example.php
+++ docExamples/classesAndObects/introduction/example.php
@@ -10,14 +10,34 @@ class Foo
     }
 }
 
+class OtherClass
+{
+    public $var = 'aMemberVar';
+    public function func()
+    {
+        return 'aMemberVar';
+    }
+}
+
+$element = 'aMemberVar';
+
 function getVarName()
 {
     return 'aMemberVar';
 }
 
 $foo = new Foo;
+$otherObj = new OtherClass;
 
-$element = 'aMemberVar';
+define(MYCONSTANT, 'aMemberVar');
 
-var_dump($foo->$element);
-var_dump($foo->{getVarName()});
+var_dump($foo->$element . ' from self property');
+var_dump($foo->{ getVarName() } . ' from outer function');
+var_dump($foo->{ MYCONSTANT } . ' from outer constant');
+var_dump($foo->{ 'aMemberVar' } . 'from outer literal property');
+
+var_dump($foo->{ $otherObj->var } . ' from other object property');
+var_dump($foo->{ $otherObj->func() } . ' from other object method');
+
+var_dump($foo->{ 'aMemberFunc' }() . 'from outer literal like function');
+var_dump($foo->{ $foo->aFuncName }() . 'from inner property, inner method');
