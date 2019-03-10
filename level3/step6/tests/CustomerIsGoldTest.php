4a834a83f53b03cf31bd16445969de9eda87d927
Artem Yurev <artemon-yrev@yandex.ru>
2019-03-10 20:51:55 +0300

    level3/step6/example1

---
+++ level3/step6/tests/CustomerIsGoldTest.php
@@ -0,0 +1,12 @@
+<?php
+class CustomerIsGoldTest extends PHPUnit\Framework\TestCase
+{
+    /** @test */
+    function a_customer_is_gold_if_they_have_the_respective_type()
+    {
+        $specification = new CustomerIsGold;
+        $goldCustomer = new Customer('gold');
+        $this->assertTrue($specification->isSatisfiedBy($goldCustomer));
+    }
+}
+
