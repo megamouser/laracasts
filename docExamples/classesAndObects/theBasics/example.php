054c153db663089e7c87558dac87ffbeaae95de7
Artem <artemon-yrev@yandex.ru>
2019-03-11 18:35:12 +0300

    docExamples/example4

--- docExamples/classesAndObects/theBasics/example.php
+++ docExamples/classesAndObects/theBasics/example.php
@@ -1,9 +1,31 @@
 <?php
-class SimpleClass
+class A 
 {
-    public $var = 'a default value';
-    public function displayVar()
+    function foo()
     {
-        echo $this->var;
+        if(isset($this))
+        {
+            var_dump('$this is defined (' . get_class($this) . ')'); 
+        } else {
+            var_dump('$this is not defined');
+        }
     }
 }
+
+class B
+{
+    function bar()
+    {
+        A::foo();
+    }
+}
+
+$a = new A;
+$a->foo();
+
+A::foo();
+
+$b = new B;
+$b->bar();
+
+B::bar();
